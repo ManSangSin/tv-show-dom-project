50e632cff96925b3017bd18e1a2e5be9645cf218
Man Sang Sin <man.sang.sin@gmail.com>
2023-05-06 11:35:35 +0100

    list each episode title and img
    - added div element
    - grabbed title and image from object

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  allEpisodesContainerElement = document.querySelector("#allEpisodesContainer");
+  episodeList.forEach((episode) => {
+    episodeContainer = document.createElement("div");
+    episodeTitleElement = document.createElement("h2");
+    episodeTitleElement.innerText = episode.name;
+    episodeImgElement = document.createElement("img");
+    episodeImgElement.src = episode.image.medium;
+    episodeContainer.append(episodeTitleElement, episodeImgElement);
+    allEpisodesContainerElement.append(episodeContainer);
+  });
+  console.log(episodeList);
 }
 
 window.onload = setup;
