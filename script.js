b6c218b7d11e77d1ccd321fc0367ddc430a69194
Man Sang Sin <man.sang.sin@gmail.com>
2023-05-07 08:35:15 +0100

    Level 100
    - completed requirements for level 100

--- script.js
+++ script.js
@@ -6,18 +6,45 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  allEpisodesContainerElement = document.querySelector("#allEpisodesContainer");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let allEpisodesContainerElement = document.querySelector(
+    "#allEpisodesContainer"
+  );
   episodeList.forEach((episode) => {
-    episodeContainer = document.createElement("div");
-    episodeTitleElement = document.createElement("h2");
-    episodeTitleElement.innerText = episode.name;
-    episodeImgElement = document.createElement("img");
+    let episodeContainer = document.createElement("div");
+    episodeContainer.classList.add("episodeCard");
+    let episodeVar = episode.number;
+    if (episodeVar < 10) {
+      episodeVar = `0${episode.number}`;
+    }
+    let seasonVar = episode.season;
+    if (seasonVar < 10) {
+      seasonVar = `0${episode.season}`;
+    }
+    let episodeTitleElement = document.createElement("h2");
+    episodeTitleElement.innerText = `${episode.name} - S${seasonVar}E${episodeVar}`;
+    episodeTitleElement.classList.add("border");
+    let episodeImgElement = document.createElement("img");
     episodeImgElement.src = episode.image.medium;
-    episodeContainer.append(episodeTitleElement, episodeImgElement);
+    let episodeSummaryElement = document.createElement("p");
+    episodeSummaryElement.classList.add("margin");
+    episodeSummaryElement.innerText = episode.summary
+      .replaceAll("<p>", "")
+      .replaceAll("</p>", "")
+      .replaceAll("<br>", "");
+    episodeContainer.append(
+      episodeTitleElement,
+      episodeImgElement,
+      episodeSummaryElement
+    );
     allEpisodesContainerElement.append(episodeContainer);
   });
+  let copyrightElement = document.createElement("p");
+  copyrightElement.innerText = "Data came from TVMaze.com";
+  footerElement = document.querySelector("footer");
+  footerElement.append(copyrightElement);
   console.log(episodeList);
+  console.log(footerElement);
 }
 
 window.onload = setup;
